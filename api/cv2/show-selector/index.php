1f497e7f454b5537764d7e4fab9f00b48aa113b5
Theodor Boshkoski <81684355+PopularTopplingJelly@users.noreply.github.com>
2024-03-06 22:47:46 +0100

    fix timestamps

--- api/cv2/show-selector/index.php
+++ api/cv2/show-selector/index.php
@@ -171,7 +171,7 @@
                                                                 				$result_dlcimg = array_filter($arr, function($obj)use($id){return !empty($obj['id']) && $obj['id'] === $id;});
                                                                 				$key_4 = key($result_dlcimg);
 												$dlcimg = $result_dlcimg[$key_4]["dlc_item"]["base"] . $result_dlcimg[$key_4]["dlc_item"]["path"];
-												$actual_show_fr = ["id" => $beta->id, "show_name" => $show_name, "show_desc" => $show_desc,  "begins" => $starts_at, "ends" => $ends_at, "roundpool" => $beta->default_episode, "image" => $dlcimg];
+												$actual_show_fr = ["id" => $beta->id, "show_name" => $show_name, "show_desc" => $show_desc,  "begins" => $starts_at + 3600, "ends" => $ends_at + 3600, "roundpool" => $beta->default_episode, "image" => $dlcimg];
 												$shows_local[$beta->id] = $actual_show_fr;
 											}
 											else{
