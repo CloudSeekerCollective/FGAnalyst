af31b063b903850985896a7cab3199a9a072326c
Theodor Boshkoski <81684355+PopularTopplingJelly@users.noreply.github.com>
2024-03-28 17:16:43 +0100

    newsfeeds are out :pog:

---
+++ cv2/newsfeed/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html data-bs-theme="light">
+	<head>
+		<title>CV2 / CloudSeeker</title>
+		<script src='/jquery-3.6.0.min.js'></script>
+		<link href="/bootstrap.min.css" rel="stylesheet">
+		<script src="/bootstrap.bundle.min.js"></script>
+		<script src="/popper.min.js"></script>
+		<link href="/ChatroomsClient.css" rel="stylesheet">
+		<script src="../ChatroomsClient.js"></script>
+		<meta property="og:type" content="website">
+	 	<meta property="og:description" content="CV2 is a Fall Guys content viewer and downloader made by The CloudSeeker Collective.">
+	 	<meta property="og:title" content="CV2 / CloudSeeker">
+	 	<meta property="og:url" content="https://cloudseeker.xyz">
+		<link rel="manifest" href="/cv2/manifest.json">
+		<script>var currentResource = "news";</script>
+	</head>
+<body>
+<!-- style='background: pink !important;' -->
+<?php include("../header.php"); ?>
+<br>
+<div class="container text-center" id="cv2-base">
+	<h1>Newsfeeds</h1>
+	<div class="cv2-download-loading"></div>
+<div id="cv2_newsfeeds_carousel" class="carousel slide bg-dark">
+  <div class="carousel-indicators">
+  </div>
+  <div id="cv2_newsfeeds" class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" alt="..." src="https://fallguys-assets.gamefuel.info/fallguys-uploads/zGfxphOvpFjo8XUqmGew/uploads/newsfeed_ui_corncrake.png">
+      <div class="carousel-caption rounded text-white" style="background: rgba(25,135,84,0.5)">
+        <h2>AVAILABLE NOW: THE WORLD EATER</h2>
+  <h3><span class="badge rounded-pill text-bg-dark">Ends At: 17th April</span></h3>
+        <p>Forget guard duty - the mighty dragon Alduin swoops into the store today!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#cv2_newsfeeds_carousel" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#cv2_newsfeeds_carousel" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+	<hr>
+	<div class="alert alert-warning">More features are coming soon! Follow us on <a href="https://twitter.com/CloudSeekerEN" target="_blank">Twitter/X</a> for more CV2 updates!</div>
+	<div class="copyright">CV2 is a Fall Guys content viewer and downloader made by The CloudSeeker Collective.<br>
+	The CloudSeeker Collective is not affiliated with Mediatonic and Epic Games. Fall Guys and the "Fall Guys" characters are registered trademarks of Mediatonic and Epic Games.<br>
+	<b>All information on this website is subject to change.</b></div>
+</div>
+</div>
+</div class='specialGuest'></div>
+</body>
+</html>
