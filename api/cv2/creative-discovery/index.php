ff34f83329e584a1ab64b6b755a2b40c23baef84
Theodor Boshkoski <81684355+PopularTopplingJelly@users.noreply.github.com>
2024-06-21 11:41:48 +0200

    Update index.php

--- api/cv2/creative-discovery/index.php
+++ api/cv2/creative-discovery/index.php
@@ -5,6 +5,106 @@
 	header("Content-Type: application/json");
 	header("X-Powered-By: CloudSeeker CV2");
 	include("../connect.php");
+	$debug = array();
+
+	if(mt_rand(1,1500) == 1){
+		header("HTTP/2 418 I'm a teapot");
+		crashWithErrorCode("Did PTJ seriously plug in the teapot instead of the server?", "x_P_4180");
+	}
+
+	function triggerErrorFailsafe($error, $errorCode, $extra){
+		header("Cache-Control: no-store, must-revalidate");
+		if(file_exists("../latest_content")){
+			echo('{"xstatus":"successWithPrecautions","download":"https://cloudseeker.xyz/api/cv2/download-direct/'. json_decode(file_get_contents("../latest_content"))->version .'.json","contentVersion":"'. json_decode(file_get_contents("../latest_content"))->version .'","notice":"The robots behind the scenes could not download the latest Fall Guys content file, so this file right here is the latest file known to CV2.", "debug":'. json_encode(["error" => $error, "errorCode" => $errorCode, "extra" => $extra]) .'}');
+			exit;
+		}
+		else{
+			crashWithErrorCode($error, $errorCode);
+		}
+	}
+
+	$headers = array("X-Unity-Version: ". $_X_UNITY_VERSION, "Content-Type: application/json");
+	$content = '{"type":"EosSignIn","token":"'. $_EOS_ACCOUNT_TOKEN .'","properties":null,"userParameters":{"lang":"'. $lang .'","locale":"'. $loc .'"},"clientVersion":"'. $_GAME_VERSION .'","clientVersionSignature":"'. $_CLIENT_SIG .'","platform":"win","contentBranch":null}';
+
+	$curl_inst = curl_init();
+
+	curl_setopt_array($curl_inst, array(
+		CURLOPT_URL => $_CATAPULT_LOGIN_URL,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => '{"type":"EosSignIn","token":"'. $_EOS_ACCOUNT_TOKEN .'","properties":null,"userParameters":{"lang":"'. $lang .'","locale":"'. $loc .'"},"clientVersion":"'. $_GAME_VERSION .'","clientVersionSignature":"'. $_CLIENT_SIG .'","platform":"win","contentBranch":null}',
+		CURLOPT_HTTPHEADER => array("X-Unity-Version: ". $_X_UNITY_VERSION, "Content-Type: application/json", "User-Agent: UnityPlayer/". $_X_UNITY_VERSION ." (UnityWebRequest/1.0, libcurl/7.84.0-DEV)")
+	));
+
+	$curl_res = curl_exec($curl_inst);
+	$curlinfo = curl_getinfo($curl_inst);
+	curl_close($curl_inst);
+
+	if($curl_res == false){
+		triggerErrorFailsafe("Could not connect to the Fall Guys server at this moment", "x_C_4200");
+	}
+	$curl_done = json_decode((string)$curl_res);
+	if(empty($curl_done->contentUrl)){
+		triggerErrorFailsafe("Could not connect to the Fall Guys server at this moment", "x_C_4300", $curlinfo);
+	}
+	$cv2_download_link = $curl_done->contentUrl;
+	$cv2_current = fopen("../../../../fg_response.json", "w+");
+	fwrite($cv2_current, json_encode($curl_done));
+
+	if(!file_exists($cv2_lang . "/" . $curl_done->contentVersion . ".json")){
+		header("Cache-Control: no-store, must-revalidate");
+		$curl_cv2 = curl_init();
+		curl_setopt($curl_cv2, CURLOPT_URL, $cv2_download_link);
+		curl_setopt($curl_cv2, CURLOPT_USERAGENT, "User-Agent: UnityPlayer/". $_X_UNITY_VERSION ." (UnityWebRequest/1.0, libcurl/7.84.0-DEV)");
+		curl_setopt($curl_cv2, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl_cv2, CURLOPT_RETURNTRANSFER, true);
+
+		$curl_cv2_res = curl_exec($curl_cv2);
+
+		if($curl_cv2_res == false){
+			crashWithErrorCode("Content file could not be downloaded", "x_F_4010");
+		}
+		$cv2_current = fopen($cv2_lang . "/" . $curl_done->contentVersion . ".json", "w+");
+		fwrite($cv2_current, $curl_cv2_res);
+	}
+	else{
+		$curl_cv2_res = file_get_contents($cv2_lang . "/" . $curl_done->contentVersion . ".json");
+	}
+	if(file_exists("../latest_content")){
+		file_put_contents("../latest_content", '{"version":"'. $curl_done->contentVersion .'"}');
+	}
+	$return_object = [
+		"xstatus" => "success",
+		"locale" => $loc,
+		"notice" => null,
+		"download" => "https://cloudseeker.xyz/api/cv2/download-direct/" . $cv2_lang . "/" . $curl_done->contentVersion . ".json",
+		"contentVersion" => $curl_done->contentVersion,
+		"environment" => [
+			"environment_id" => $_CATAPULT_ENVIRONMENT,
+			"game_version" => $_GAME_VERSION,
+			"client_signature" => $_CLIENT_SIG
+		],
+		"debug" => $debug
+	];
+	if($_HAS_SITEWIDE_ANNOUNCEMENT){
+		$return_object["notice"] = $_SITEWIDE_ANNOUNCEMENT_CONTENTS;
+	}
+	echo(json_encode($return_object));
+
+?>
+root@mail:/home/user-data/www/cloudseeker.xyz/api/cv2# cat creative-discovery/index.php 
+<?php
+
+	// CV2: A free and open source Fall Guys content viewing and downloading beacon created by The CloudSeeker Collective (https://cloudseeker.xyz) <admin@cloudseeker.xyz>.
+
+	header("Content-Type: application/json");
+	header("X-Powered-By: CloudSeeker CV2");
+	include("../connect.php");
 	$debug = array("thing" => time() >= strtotime($_EOS_EXPIRE));
 
 	function triggerErrorFailsafe($error, $errorCode){
@@ -97,12 +197,30 @@
 		$sec_name = explode(".", $alpha->section_title);
 		$final_level_data[$discovery_query[1]] = array("section_name" => getLocalisedString($sec_name[1], $_final->localised_strings));
 		foreach($level_data[0]->levels as $beta){
+			if($_LOG_WUSHU_LEVELS){
+                		if(empty($beta->snapshot->author->name_per_platform->eos)){
+                		        // you are given one fucking job to implement epic online services properly and not even your APIs fucking work
+                		        $usernames = array_values((array)$beta->snapshot->author->name_per_platform);
+					if(!empty($usernames[0]))
+                		        	$epic_username = key((array)$beta->snapshot->author->name_per_platform) . "_" . $usernames[0];
+                			else
+						$epic_username = "";
+				}
+                		else{
+                		        $epic_username = $beta->snapshot->author->name_per_platform->eos;
+        		        }
+				$lfdl = mysqli_query($_WUSHU_ARCHIVE_DATABASE_LINK, "INSERT INTO levels (common_name, share_code, tags, playcount, likes, dislikes, min_players, max_players, author_name) VALUES ('". stripslashes(htmlspecialchars($beta->snapshot->version_metadata->title)) ."', '". $beta->snapshot->share_code ."', '". json_encode($beta->snapshot->version_metadata->creator_tags) ."', ". $beta->snapshot->stats->play_count .", ". $beta->snapshot->stats->likes .", ". $beta->snapshot->stats->dislikes .", 1, ". $beta->snapshot->version_metadata->max_player_count .", '". $epic_username ."') ON DUPLICATE KEY UPDATE common_name = VALUES(common_name),tags = VALUES(tags),playcount = VALUES(playcount),likes = VALUES(likes),dislikes = VALUES(dislikes),min_players = VALUES(min_players),max_players = VALUES(max_players),author_name = VALUES(author_name);");
+        		}
+			if(!empty($beta->snapshot->images->PreviewImage[0]->url))
+				$lvl_image = $beta->snapshot->images->PreviewImage[0]->url;
+			else
+				$lvl_image = "";
 			array_push($final_level_data[$discovery_query[1]], (object)[
 				"title" => $beta->snapshot->version_metadata->title, 
 				"share_code" => $beta->snapshot->share_code, 
 				"author" => $beta->snapshot->author->name_per_platform, 
 				"description" => $beta->snapshot->version_metadata->description, 
-				"image" => $beta->snapshot->images->PreviewImage[0]->url, 
+				"image" => $lvl_image, 
 				"ratings" => (object)[
 					"playcount" => $beta->snapshot->stats->play_count,
 					"likes" => $beta->snapshot->stats->likes, 
@@ -120,6 +238,7 @@
 		"xstatus" => "success",
 		"level_data" => $final_level_data,
 		"contentVersion" => $curl_done->contentVersion,
+		"notice" => null,
 		"environment" => [
                         "environment_id" => $_CATAPULT_ENVIRONMENT,
                         "game_version" => $_GAME_VERSION,
