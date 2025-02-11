dcf067755f9894ff5f9a8ced6342487e9dde199c
Theodor Boshkoski <81684355+PopularTopplingJelly@users.noreply.github.com>
2025-02-11 21:10:51 +0100

    update all source code pt1

--- api/cv2/show-selector/index.php
+++ api/cv2/show-selector/index.php
@@ -16,7 +16,7 @@
 			$content_version = json_decode(file_get_contents("../latest_content"))->version;
 			//echo($errorCode);
                         //echo('{"xstatus":"successWithPrecautions","download":"https://cloudseeker.xyz/api/cv2/download-direct/'. json_decode(file_get_contents("../latest_content"))->version>
-                        //$curl_cv2_res = file_get_contents("../download-direct/". $failsafeLoc ."/". $content_version .".json");
+                        //$curl_cv2_res = file_get_contents("../download-direct/". $failsafeLoc ."/". $content_file);
                 }
                 else{
                         crashWithErrorCode($error, $errorCode);
@@ -59,8 +59,13 @@
 		$cv2_download_link = $curl_done->contentUrl;
 		$content_version = $curl_done->contentVersion;
 	}
+	$content_file = $curl_done->contentVersion . ".json";
+        if(isset($_GET["mobile"])){
+                $content_file = $curl_done->contentVersion . "_M.json";
+        }
+
 	try{
-		if(!file_exists("../download-direct/". $cv2_lang ."/". $content_version . ".json") and $should_try == true or empty(file_get_contents("../download-direct/". $cv2_lang ."/". $content_version . ".json")) and $should_try == true){
+		if(!file_exists("../download-direct/". $cv2_lang ."/". $content_file) and $should_try == true or empty(file_get_contents("../download-direct/". $cv2_lang ."/". $content_file)) and $should_try == true){
 			header("Cache-Control: no-store, must-revalidate");
 			$curl_cv2 = curl_init();
 			curl_setopt($curl_cv2, CURLOPT_URL, $cv2_download_link);
@@ -69,19 +74,19 @@
 			curl_setopt($curl_cv2, CURLOPT_RETURNTRANSFER, true);
 
 			$curl_cv2_res = curl_exec($curl_cv2);
-			$cv2_current = fopen("../download-direct/". $cv2_lang ."/". $content_version . ".json", "w+");
+			$cv2_current = fopen("../download-direct/". $cv2_lang ."/". $content_file, "w+");
 			fwrite($cv2_current, $curl_cv2_res);
 			if($curl_cv2_res == false){
 				crashWithErrorCode("Content file could not be downloaded", "x_F_4010");
 			}
 		}
-		$curl_cv2_res = file_get_contents("../download-direct/". $cv2_lang ."/". $content_version .".json");
+		$curl_cv2_res = file_get_contents("../download-direct/". $cv2_lang ."/". $content_file);
 		$_final = json_decode($curl_cv2_res);
 		$shows = array();
 		$shows_local_2 = array();
 
-		if(file_exists("../download-direct/" . $content_version . "-shows-". $cv2_lang .".json")){
-                        $cached_show_list = json_decode(file_get_contents("../download-direct/" . $content_version . "-shows-". $cv2_lang .".json"));
+		if($_CV2_USE_ARCHIVED and file_exists("../download-direct/archive/" . $content_version . "-shows-". $cv2_lang .".json")){
+                        $cached_show_list = json_decode(file_get_contents("../download-direct/archive/" . $content_version . "-shows-". $cv2_lang .".json"));
 			$shows["live_shows"] = array();
 			foreach($cached_show_list->live_shows as $gamma){
 				$shows_local = array();
@@ -111,9 +116,42 @@
 
 		foreach($_final->show_selector_sections as $x){
 			// dont mind me either
-			if(!empty($x->section->gates)){
+			$gates;
+			switch($x->section->type){
+				case "cms_shows":
+					$gates = $x->section->gates;
+				break;
+				case "cms_show_entry_points":
+					$gates = [];
+					foreach($x->section->show_entry_points as $points){
+						$arr = json_decode(json_encode($_final->show_entry_point), true);
+						$point_id = explode(".", $points->show_entry_point);
+						$point_id = $point_id[1];
+						// fortnite got me like
+						$result_zeropoint = array_filter($arr, function($obj)use($point_id){return !empty($obj['id']) && $obj['id'] === $point_id;});
+						$key_5 = key($result_zeropoint);
+						// wtflip mediatonic
+						$entrypoints = [];
+						switch($result_zeropoint[$key_5]["entry_point"]["type"]){
+							case "nested":
+								foreach($result_zeropoint[$key_5]["entry_point"]["nested_gates_entry"]["gates"] as $iranoutofletters){
+									array_push($gates, (object)["gate" => $iranoutofletters["gate"]]);
+								}
+							break;
+							case "individual":
+								array_push($gates, (object)["gate" => $result_zeropoint[$key_5]["entry_point"]["individual_gate_entry"]["gate"]]);
+							break;
+						}
+						//$gates += $entrypoints;
+					}
+				break;
+				default:
+				continue 2;
+			}
+			//var_dump($gates);
+			if(!empty($gates)){
 				$shows_local = array();
-				foreach($x->section->gates as $y){
+				foreach($gates as $y){
 					$show_id = explode(".", $y->gate);
 					$z_count = 0;
 					$s_count = 0;
@@ -135,6 +173,31 @@
 											//array_push($debug, ["z" => $z, "beta" => $beta]);
 											$rewards_id = $beta->episode_reward_settings_id ?? "";
 											$rewards = null;
+											$stag_name = null;
+											$stag_icon = null;
+											$_stag = null;
+											// there aren't many show tags so using a foreach for this in big 2024 shouldn't hurt
+											if(!empty($beta->show_tag)){
+												foreach($_final->show_tags as $stag){
+													if($stag->id == $beta->show_tag){
+														//var_dump($stag);
+														//exit;
+														$arr = json_decode(json_encode($_final->dlc_images), true);
+		                                                                				if(!empty($stag->tag_icon)){
+															$id = explode(".", $stag->tag_icon);
+															$id = $id[1];
+														}
+														$tagname_loc_id = explode(".", $stag->tag_name);
+														$tagname_loc_id = $tagname_loc_id[1];
+		                                                                				$result_dlcimg2 = array_filter($arr, function($obj)use($id){return !empty($obj['id']) && $obj['id'] === $id;});
+		                                                                				$key_5 = key($result_dlcimg2);
+														$stag_icon = $result_dlcimg2[$key_5]["dlc_item"]["base"] . $result_dlcimg2[$key_5]["dlc_item"]["path"];
+														$stag_name = getLocalisedString($tagname_loc_id, $_final->localised_strings);
+														$_stag = ["name" => $stag_name, "icon" => $stag_icon];
+														//var_dump($stag);
+													}
+												}
+											}
 											if(!empty($rewards_id)){
 												foreach($_final->settings_episode_rewards as $one){
 													if($one->id == $rewards_id){
@@ -145,11 +208,11 @@
 											if(empty($z->config->starts_at))
 												$starts_at = null;
 											else
-												$starts_at = strtotime($z->config->starts_at) + 3600;
+												$starts_at = strtotime($z->config->starts_at);
 											if(empty($z->config->ends_at))
 												$ends_at = strtotime("2100-01-01T00:00:00");
 											else
-												$ends_at = strtotime($z->config->ends_at) + 3600;
+												$ends_at = strtotime($z->config->ends_at);
 											if(!empty($starts_at) and $ends_at > time()){
 												$d_count = 0;
 												$actual_show = $_final->shows[$show_info_3];
@@ -180,8 +243,10 @@
                                                                 				$result_dlcimg = array_filter($arr, function($obj)use($id){return !empty($obj['id']) && $obj['id'] === $id;});
                                                                 				$key_4 = key($result_dlcimg);
 												$dlcimg = $result_dlcimg[$key_4]["dlc_item"]["base"] . $result_dlcimg[$key_4]["dlc_item"]["path"];
-												$actual_show_fr = ["id" => $beta->id, "show_name" => $show_name, "show_desc" => $show_desc,  "begins" => $starts_at, "ends" => $ends_at, "roundpool" => $beta->default_episode, "image" => $dlcimg, "victory_rewards" => $rewards];
+												$actual_show_fr = ["id" => $beta->id, "show_name" => $show_name, "show_desc" => $show_desc,  "begins" => $starts_at, "ends" => $ends_at, "roundpool" => $beta->default_episode, "image" => $dlcimg, "victory_rewards" => $rewards, "tag" => $_stag, "ready_up_allowed" => $beta->can_play_again ?? false, "lobby_sizes" => ["default" => $beta->size, "customs" => $beta->private_lobby_size]];
 												$shows_local[$beta->id] = $actual_show_fr;
+												$arr = [];
+												$_stag = [];
 											}
 											else{
 												$b_count++;
@@ -212,13 +277,15 @@
 
 		foreach($_final->private_lobbies_shows_tabs as $x){
 			// dont mind me either
-			if($x->id == "alternative" or !$x->is_fraggle and !$x->is_favourite){
+			$isfraggle = $x->is_fraggle ?? false;
+			$is_favourite = $x->is_favourite ?? false;
+			if($x->id == "alternative" or !$isfraggle and !$is_favourite){
 				$shows_local_customs = array();
 				foreach($_final->private_lobbies_shows as $y){
 					$show_id = explode(".", $y->show);
 					$z_count = 0;
 					$s_count = 0;
-					if($y->tab != "private_lobbies_shows_tabs.alternative" or !$y->is_active)
+					if($y->tab != "private_lobbies_shows_tabs.alternative")
 						continue;
 					foreach($_final->shows as $beta){
 						if($beta->id == $show_id[1]){
@@ -248,10 +315,14 @@
 							}
 							$arr = json_decode(json_encode($_final->dlc_images), true);
                                                         $id = $beta->show_image_cms;
+							$is_active = true;
+							if(empty($y->is_active)){
+								$is_active = false;
+							}
                                                         $result_dlcimg = array_filter($arr, function($obj)use($id){return !empty($obj['id']) && $obj['id'] === $id;});
                                                         $key_4 = key($result_dlcimg);
 							$dlcimg = $result_dlcimg[$key_4]["dlc_item"]["base"] . $result_dlcimg[$key_4]["dlc_item"]["path"];
-							$actual_show_fr = ["id" => $beta->id, "show_name" => $show_name, "show_desc" => $show_desc, "roundpool" => $beta->default_episode, "image" => $dlcimg];
+							$actual_show_fr = ["id" => $beta->id, "show_name" => $show_name, "show_desc" => $show_desc, "roundpool" => $beta->default_episode, "image" => $dlcimg, "is_active" => $is_active, "lobby_sizes" => ["default" => $beta->size, "customs" => $beta->private_lobby_size]];
 							$shows_local_customs[$beta->id] = $actual_show_fr;
 						}
 						else{
@@ -272,7 +343,7 @@
 		header("HTTP/2 500 Internal Server Error");
 		crashWithErrorCode("Internal server error", "x_P_5000");
 	}
-	file_put_contents("../download-direct/" . $content_version . "-shows-" . $cv2_lang . ".json", json_encode($shows));
+	file_put_contents("../download-direct/archive/" . $content_version . "-shows-" . $cv2_lang . ".json", json_encode($shows));
 	$return_object = [
                 "xstatus" => "success",
                 "shows" => $shows,
